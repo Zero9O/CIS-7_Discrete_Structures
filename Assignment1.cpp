b44568df91c0aeb25c055680ca61130fa477ab15
Enrique Villavisencio <zero9o@live.com>
2018-11-15 00:00:52 -0800

    Update Assignment1.cpp

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -6,75 +6,86 @@ using namespace std;
 int main()
 {
 	string input;
+	char repeat;
 
-	cout << "Enter a string (Only use ^, V, !, ->, A-Z): ";
-	getline(cin, input);
-
-	bool lastWasAlpha = false;
-	bool lastWasNot = false;
-	bool lastWasConnector = false;
-	bool isValidWff = true;
-	bool lastWasPart = false;
-
-	for (unsigned int i = 0; i < input.length(); i++)
+	do
 	{
-		char c = input[i];
-		if (c == ' ')
-			continue;
-		if (c == '^' || c == '!' || toupper(c) == 'V' || c == '-' || c == '>')
+		system("cls");
+
+		cout << "Enter a string (Only use ^, V, !, ->, A-Z): ";
+		getline(cin, input);
+
+		bool lastWasAlpha = false;
+		bool lastWasNot = false;
+		bool lastWasConnector = false;
+		bool isValidWff = true;
+		bool lastWasPart = false;
+
+		for (unsigned int i = 0; i < input.length(); i++)
 		{
-			if (c != '!')
+			char c = input[i];
+			if (c == ' ')
+				continue;
+			if (c == '^' || c == '!' || toupper(c) == 'V' || c == '-' || c == '>')
 			{
-				if (!lastWasAlpha || lastWasPart)
+				if (c != '!')
 				{
-					if (c == '>')
+					if (!lastWasAlpha)
 					{
-						lastWasConnector = true;
+						if (c == '>')
+						{
+							lastWasConnector = true;
+							break;
+						}
+						else
+						{
+							isValidWff = false;
+							break;
+						}
+					}
+					else if (c == '-')
+					{
+						lastWasAlpha = false;
 						break;
 					}
 					else
+						lastWasConnector = true;
+				}
+				else
+				{
+					if (lastWasAlpha)
 					{
 						isValidWff = false;
 						break;
 					}
+					lastWasNot = true;
+					continue;
 				}
-				else if (c == '-')
-				{
-					lastWasPart = true;
-					break;
-				}
-				else
-					lastWasConnector = true;
+				lastWasAlpha = false;
 			}
-			else
+			else if (isalpha(c))
 			{
 				if (lastWasAlpha)
-				{	
+				{
 					isValidWff = false;
 					break;
 				}
-				lastWasNot = true;
+				lastWasAlpha = true;
 				continue;
 			}
-			lastWasAlpha = false;
-		}
-		else if (isalpha(c))
-		{
-			if (lastWasAlpha)
+			else
 			{
 				isValidWff = false;
 				break;
 			}
-			lastWasAlpha = true;
-			continue;
 		}
-		else
-		{
-			isValidWff = false;
-			break;
-		}
-	}
-	cout << (isValidWff ? "That is a WFF" : "NOT Valid WFF") << endl;
+		cout << (isValidWff ? "That is a WFF" : "NOT Valid WFF") << endl;
+		cout << "Do you want to try another one?(Y or N) ";
+		cin >> repeat;
+
+		cin.ignore();
+
+	}while (toupper(repeat) == 'Y');
 
 	return 0;
 }
